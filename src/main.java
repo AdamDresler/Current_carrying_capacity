3c28497c666b2267482748cc5d6c675b33d65682
Adam Dresler <dresio80@gmail.com>
2019-12-12 19:50:31 +0100

    Add getIcc method

--- src/main.java
+++ src/main.java
@@ -1,20 +1,66 @@
 public class main {
     public static void main(String[] args) {
-        double res = Icc("A1", 0, 6);
+        String laying = "A1";
+        String insulation = "PVC";
+        int numberOfPhases = 0;
+        int cableMaterial =0;
+        double crossSection = 6.0;
+
+        double res = getIcc(laying, insulation, numberOfPhases, cableMaterial, crossSection);
 
         System.out.println(res);
+        System.out.println(is1phase(numberOfPhases));
+    }
+
+    public static boolean is1phase(int pos) {
+        boolean result = true;
+        if (pos > 0) {
+            return false;
+        }
+        return result;
     }
 
 
-    public static double Icc(String laying, int cableMaterial, double crossSection) {
+    public static double getIcc(String laying, String insulation, int numberOfPhases, int cableMaterial, double crossSection) {
         double Icc = 0.0;
-        switch (laying) {
-            case "A1": Icc = CurrentCapacityByIEC.Iz_A1_PVC1(cableMaterial, crossSection);
-                break;
+        if (is1phase(numberOfPhases)) {
+            if (insulation.equals("PVC")) {
+                switch (laying) {
+                    case "A1":
+                        Icc = CurrentCapacityByIEC.Iz_A1_PVC1(cableMaterial, crossSection);
+                        break;
+                    case "A2":
+                        Icc = CurrentCapacityByIEC.Iz_A2_PVC1(cableMaterial, crossSection);
+                        break;
+                }
+            }else{
+                switch (laying) {
+                    case "A1":
+                        Icc = CurrentCapacityByIEC.Iz_A1_XLPE1(cableMaterial, crossSection);
+                        break;
+                    case "A2":
+                        Icc = CurrentCapacityByIEC.Iz_A2_XLPE1(cableMaterial, crossSection);break;
+                }
+
+            }
+
+        } else {
+            if (insulation.equals("PVC")) {
+                switch (laying) {
+                    case "A1":
+                        Icc = CurrentCapacityByIEC.Iz_A1_PVC3(cableMaterial, crossSection);
+                        break;
+                    case "A2":
+                        Icc = CurrentCapacityByIEC.Iz_A2_PVC3(cableMaterial, crossSection);
+                        break;
+                }
+            }else{
+                //TODO
+            }
 
         }
         return Icc;
     }
-
-
 }
+
+
